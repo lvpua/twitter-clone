509f2faad950b2946db35e8af5b32569d0a0d9e0
Lois Pua <lvap@Loiss-Mac-mini-2.local>
2022-05-17 18:47:20 -0500

    init commit

---
+++ build/precache-manifest.a04f382cb1e49cfce4f2d31cd1276d48.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d9bf79f193c47c2b3554efd0ec89fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fd2e564fb6776df3158e",
+    "url": "/static/css/main.8b421d16.chunk.css"
+  },
+  {
+    "revision": "0ede6e40fc570bad032b",
+    "url": "/static/js/2.a60b059f.chunk.js"
+  },
+  {
+    "revision": "a8de4128b679d6159aad8871b5569c6d",
+    "url": "/static/js/2.a60b059f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd2e564fb6776df3158e",
+    "url": "/static/js/main.2b021062.chunk.js"
+  },
+  {
+    "revision": "723b8de4144d7b4aea6d",
+    "url": "/static/js/runtime-main.562f75cf.js"
+  }
+]);
